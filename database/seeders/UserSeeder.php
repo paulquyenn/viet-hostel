56d277385e278a03995fd52a054f6fe88850a5c9
Nguyễn Văn Quyến <paulquyenn@gmail.com>
2025-06-15 05:08:52 +0700

    feat: Add multiple landlords, tenants, and admin assistants to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -40,5 +40,108 @@ class UserSeeder extends Seeder
         ]);
 
         $tenant->assignRole('tenant');
+
+        // Thêm nhiều landlord (chủ trọ)
+        $landlords = [
+            [
+                'name' => 'Nguyễn Văn Minh',
+                'email' => 'minhnv@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Trần Thị Hoa',
+                'email' => 'hoatt@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Lê Quang Dũng',
+                'email' => 'dunglq@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Phạm Thị Mai',
+                'email' => 'maipt@gmail.com',
+                'password' => bcrypt('12345678')
+            ]
+        ];
+
+        foreach ($landlords as $landlordData) {
+            $newLandlord = User::firstOrCreate([
+                'email' => $landlordData['email']
+            ], $landlordData);
+            $newLandlord->assignRole('landlord');
+        }
+
+        // Thêm nhiều tenant (người thuê)
+        $tenants = [
+            [
+                'name' => 'Võ Thị Lan',
+                'email' => 'lanvt@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Hoàng Văn Tú',
+                'email' => 'tuhv@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Đặng Thị Nga',
+                'email' => 'ngadt@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Bùi Văn Khang',
+                'email' => 'khangbv@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Lý Thị Thanh',
+                'email' => 'thanhlt@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Phan Văn Đức',
+                'email' => 'ducpv@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Ngô Thị Hương',
+                'email' => 'huongnt@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Vũ Văn Long',
+                'email' => 'longvv@gmail.com',
+                'password' => bcrypt('12345678')
+            ]
+        ];
+
+        foreach ($tenants as $tenantData) {
+            $newTenant = User::firstOrCreate([
+                'email' => $tenantData['email']
+            ], $tenantData);
+            $newTenant->assignRole('tenant');
+        }
+
+        // Thêm admin phụ
+        $adminAssistants = [
+            [
+                'name' => 'Quản trị viên 2',
+                'email' => 'admin2@gmail.com',
+                'password' => bcrypt('12345678')
+            ],
+            [
+                'name' => 'Support Admin',
+                'email' => 'support@gmail.com',
+                'password' => bcrypt('12345678')
+            ]
+        ];
+
+        foreach ($adminAssistants as $adminData) {
+            $newAdmin = User::firstOrCreate([
+                'email' => $adminData['email']
+            ], $adminData);
+            $newAdmin->assignRole('admin');
+        }
     }
 }
